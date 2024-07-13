67890f3fee7dfb96dfb711662eabed94efd27c1b
Abhishek Peiris <abhisheklpeiris@gmail.com>
2024-07-13 18:18:02 +0530

    get all posts

--- aora/lib/useAppwrite.js
+++ aora/lib/useAppwrite.js
@@ -1,26 +1,29 @@
-import { useEffect, useState } from "react";
 import { Alert } from "react-native";
+import { useEffect, useState } from "react";
 
 const useAppwrite = (fn) => {
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        setIsLoading(true);
-        try {
-          const response = await fn();
-          setData(response);
-        } catch (error) {
-          Alert.alert('Error', error.message)
-        } finally {
-          setIsLoading(false);
-        }
-      }
-      fetchData();
-    }, []);
-  
-    return { data }
-}
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const res = await fn();
+      setData(res);
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => fetchData();
+
+  return { data, loading, refetch };
+};
 
 export default useAppwrite;
